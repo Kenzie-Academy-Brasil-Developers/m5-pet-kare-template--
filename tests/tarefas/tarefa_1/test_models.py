4966c5cd12411158667c50e4db6cd8a7aaadb66c
Chrystian Rodolfo <87505223+kenzie-chrystian@users.noreply.github.com>
2023-02-06 16:15:11 -0300

    moved traits test

--- tests/tarefas/tarefa_1/test_models.py
+++ tests/tarefas/tarefa_1/test_models.py
@@ -41,17 +41,6 @@ class TraitModelTest(TestCase):
 
         self.assertEqual(max_length, expected, message)
 
-    @patch("django.utils.timezone.now", return_value="2022-11-27T17:55:22.819371Z")
-    def test_field_created_at_properties(self, _: MagicMock):
-        expected = timezone.now()
-        trait_data = {"name": "trait"}
-        created_trait = Trait.objects.create(**trait_data)
-
-        received = created_trait.created_at
-        message = "Verifique se o campo 'created_at' está sendo gerado exatamente no momento de criação da instância"
-
-        self.assertEqual(received, expected, message)
-
 
 class PetModelTest(TestCase):
     def test_field_name_properties(self):
