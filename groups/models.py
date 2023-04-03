49b79f33f3f10611bd00b4ad69a9bdf44f9d82e2
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-04-03 16:18:23 -0300

    feat tarefa 1 finalizada

---
+++ groups/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Group(models.Model):
+    scientific_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ pets/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class PetSex(models.TextChoices):
+    name = "Male"
+    female = "Female"
+    not_informad = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20, choices=PetSex.choices, default=PetSex.not_informad
+    )
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.RESTRICT, related_name="Pets"
+    )
