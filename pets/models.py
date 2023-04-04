134c60fa2da75494929dc1fac3a880ab80b58dd4
Joseph <josephvriesman16@gmail.com>
2023-04-04 17:14:36 -0300

    feat: task 1 finalized

---
+++ pets/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Sex(models.TextChoices):
+    MALE = "Male",
+    FEMALE = "Female",
+    NOT_INFORMED = "Not Informed"
+
+class Pet(models.Model):
+    name = models.CharField(max_length = 50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length = 20, choices = Sex.choices, default = Sex.NOT_INFORMED)
+
+    group = models.ForeignKey(
+        "groups.Group",
+        on_delete = models.PROTECT,
+        related_name = "pets"
+    )
+
+    traits = models.ManyToManyField(
+        "traits.Trait",
+        related_name="pet"
+    )
+
+    def __repr__(self) -> str:
+        return f"<Pet ({self.id} - {self.name} - {self.group} - {self.traits})>"
+
+# Create your models here.
---
+++ traits/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Trait(models.Model):
+    name = models.CharField(max_length = 20, unique = True)
+
+# Create your models here.
