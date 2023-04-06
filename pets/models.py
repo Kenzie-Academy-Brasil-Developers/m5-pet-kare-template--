79af48dd68fce2a1ec1005837d945ee46b99dba3
isac <jeandersantos0310@gmail.com>
2023-04-06 15:20:44 -0300

    feat: tarefa1 concluida

--- pets/models.py
+++ pets/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+
+class GenderChoices(models.TextChoices):
+    Male = "Male"
+    Female = "Female"
+    Default = "Not Informed"
 class Pet(models.Model):
+  
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     weight = models.FloatField()
-    sex = models.CharField(max_length=20, choices=(("Male", "Male"), ("Female", "Female"), ("Not Informed", "Not Informed")), default="Not Informed")
+    sex = models.CharField(max_length=20, choices=GenderChoices.choices, default=GenderChoices.Default)
 
-    group = models.OneToOneField("groups.Group", on_delete=models.PROTECT)
+    group = models.ForeignKey("groups.Group", on_delete=models.PROTECT, related_name="pets")
