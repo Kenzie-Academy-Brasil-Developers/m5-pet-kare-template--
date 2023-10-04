ab40b9736057e35a76d2c92d960f78a5853e468e
svnoliveira <srkuran@gmail.com>
2023-10-03 22:13:30 -0300

    feat: tarefa 1 finalizada

---
+++ pets/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class SexPet(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    NOT_INFORMED = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20,
+        choices=SexPet.choices,
+        default=SexPet.NOT_INFORMED
+    )
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.PROTECT, related_name="pets"
+    )
+    traits = models.ManyToManyField("traits.Trait", related_name="pets")
