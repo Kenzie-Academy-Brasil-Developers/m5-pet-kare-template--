ec79c9dac0bd1a6d27438c45a1be63e13d1c2d47
unknown <matheuss456@hotmail.com>
2023-11-30 13:27:54 -0300

    doc group model

--- groups/models.py
+++ groups/models.py
@@ -4,7 +4,18 @@ from django.utils import timezone
 
 # Create your models here.
 class group(models.Model):
-    """class group(models.Model):"""
+    """class group(models.Model):
+    representação dos grupos de pets.
 
-    scientific_name = models.CharField(max_length=50)
+    Atributos:
+        scientific_name: Varchar(50) unique
+        created_at: DateTimeField auto_now_add
+            - Data de quando o grupo foi criado.
+
+    Relacionamentos:
+        - pet: any to many
+
+    """
+
+    scientific_name = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=timezone.now())
