3e2de00d3b333dc692d0c0deeecdbb26b5c994cf
unknown <matheuss456@hotmail.com>
2023-11-30 13:37:57 -0300

    doc trait model

--- traits/models.py
+++ traits/models.py
@@ -4,7 +4,14 @@ from django.utils import timezone
 
 # Create your models here.
 class Trait(models.Model):
-    """class Trait(models.Model):"""
+    """class Trait(models.Model):
+
+    Atributos:
+       name: Varchar(50) unique
+       created_at: DateTimeField auto_now_add
+           - Data de quando o grupo foi criado.
+
+    """
 
     name = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=timezone.now())
