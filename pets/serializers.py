109a1a026bda62c926bcf515abc649b606bc5203
unknown <matheuss456@hotmail.com>
2023-12-01 11:01:10 -0300

    fest pet serializer no group and traits

---
+++ pets/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from pets.models import SexField
+
+
+class PetSerializer(serializers.Serializer):
+    """class PetSerializer(serializers.Serializer):"""
+
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_lenght=50)
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.CharField(
+        max_lenght=20, choices=SexField.choices, default=SexField.NOT_INFORMED
+    )
+
+    group = ...
+
+    traits = ...
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class PetView(APIView, PageNumberPagination):
+    def post(self, request: Request) -> Response:
+        ...
