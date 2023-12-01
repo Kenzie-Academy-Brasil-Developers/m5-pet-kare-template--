ad4ac8f0db60682896af51a3800348c706596827
Bruno Moleta <brunomoleta@pm.me>
2023-12-01 11:16:16 -0300

    fix/tarefa 01 Pet class add sex

--- pets/models.py
+++ pets/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 
 class PetSex(models.TextChoices):
-    MALE = "male"
-    FEMALE = "female"
-    NOT_INFORMED = "not informed"
+    MALE = "Male"
+    FEMALE = "Female"
+    NOT_INFORMED = "Not informed"
 
 
 class Pet(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     weight = models.FloatField()
-    sex = PetSex.choices,
-    default = PetSex.NOT_INFORMED
+    sex = models.CharField(
+        max_length=20,
+        choices=PetSex.choices,
+        default=PetSex.NOT_INFORMED
+    )
     group = models.ForeignKey(
         "groups.Group",
         on_delete=models.CASCADE,
