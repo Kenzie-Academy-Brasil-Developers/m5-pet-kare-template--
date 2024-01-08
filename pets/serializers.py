3453fad625f5a8cc32a085b67b654fa9e18f5682
Sheiji <andrenacamura@gmail.com>
2024-01-08 09:52:39 -0300

    feat: Create Serializers pets, group and trait

--- pets/serializers.py
+++ pets/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Sex
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
 
 class SexSerializer(serializers.Serializer):
     name = serializers.CharField(max_length = 50)
@@ -8,3 +10,15 @@ class SexSerializer(serializers.Serializer):
     sex = serializers.ChoiceField(
         choices=Sex.choices, default = Sex.OTHER
     )
+    
+class PetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only = True)
+    name = serializers.CharField(max_length = 50)
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.ChoiceField(
+        choices=Sex.choices, default = Sex.OTHER
+    )
+    group = GroupSerializer()
+    traits = TraitSerializer(many = True)
+    
